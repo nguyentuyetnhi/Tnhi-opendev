8246201f62579286cc48365b4d789f6e7237dc29
nguyentuyetnhi <99497122+nguyentuyetnhi@users.noreply.github.com>
2025-03-10 23:22:49 +0700

    fix: absolute QR

--- react-design/src/components/Registration.jsx
+++ react-design/src/components/Registration.jsx
@@ -26,31 +26,27 @@ const Registration = () => {
               Choose from the attractions of the FUN arena.
             </p>
           </div>
-          <div className="relative bg-gray-900 p-4">
+          <div className="relative bg-gray-900 p-6 flex flex-col items-center">
+            <div className="step relative z-20 p-4 bg-gray-800 text-white rounded-lg w-full max-w-md">
+              <div className="step-content flex justify-between items-center">
+                <h3 className="no-skew text-lg font-bold">Register</h3>
+                <a href="https://web.racefacer.com/kiosk/kartarenacheb/">
+                  <button className=" no-skew clickcard-btn">Click here</button>
+                </a>
+              </div>
+            </div>
             <img
               src={QR}
               alt="QR"
-              className="qr-code absolute top-1/2 right-0 transform -translate-y-1/2 translate-x-1/4 z-30"
+              className="qr-code absolute top-0 right-0 transform translate-x-1/4 -translate-y-1/3 z-30 w-24 h-24"
             />
 
-            <div className="step absolute z-20 p-4 bg-gray-800 text-white rounded-lg">
-              <div className="step-content flex justify-between items-center">
-                <h3 className="text-lg font-bold no-skew">Register</h3>
-                <div className="no-skew">
-                  <a href="https://web.racefacer.com/kiosk/kartarenacheb/">
-                    <button className="clickcard-btn">Click here</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <p className="text mt-2">
-              You can register at the reception or by<br />
-              clicking the button above from the comfort of<br />
+            <p className="text mt-4 text-center text-white">
+              You can register at the reception or <br />
+              by clicking the button above from the comfort of <br />
               your home.
             </p>
           </div>
-
 
           <div className="box mt-10">
             <div className="step">
